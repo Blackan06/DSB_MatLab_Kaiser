ef0550f26dd09b43af52c2286322de1ae836c282
AnhKiet <huynhanhkiet2222@gmail.com>
2024-03-22 19:27:26 +0700

    First Commit

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,15 @@ import os
 from app import app
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        audio_data = request.files['audio_data']
+        if audio_data:
+            audio_data.save(os.path.join(app.config['UPLOAD_FOLDER'], 'recorded_audio.ogg'))
+            return redirect(url_for('index'))
 
+    return render_template('index.html')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
